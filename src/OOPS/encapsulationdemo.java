5cae5207536d400e3801a3e2e021403398e9742e
harsh-dev-tool <tripathiharshit182000@gmail.com>
2023-02-28 12:07:13 +0530

    first

---
+++ src/OOPS/encapsulationdemo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package OOPS;
+
+/**
+ * @author Asus
+ *
+ */
+public class encapsulationdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello ");
+System.out.println("hello  shachi ");
+itn a = 5;
+	}
+
+}
